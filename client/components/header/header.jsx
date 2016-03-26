ed5b41a8af7c56df9f76e6af170a70710da1b7c0
Senott <senott@yahoo.com>
2016-03-26 19:49:46 -0300

    @DASMacBookAir: 4.8 sign in.

--- client/components/header/header.jsx
+++ client/components/header/header.jsx
@@ -1,4 +1,36 @@
 Header = React.createClass({
+  mixins: [ReactMeteorData],
+  getMeteorData(){
+    let data = {};
+    data.currentUser = Meteor.user();
+    return data;
+  },
+  getInitialState(){
+    return {
+      message: '',
+      messageClass: 'hidden'
+    }
+  },
+  displayError(message){
+    this.setState({
+      message: message,
+      messageClass: 'alert alert-danger message'
+    })
+  },
+  handleSubmit(e){
+    e.preventDefault();
+    this.setState({message: '', messageClass: 'hidden'});
+    var that = this;
+    var email = ReactDOM.findDOMNode(this.refs.email).value.trim();
+    var password = ReactDOM.findDOMNode(this.refs.password).value.trim();
+    Meteor.loginWithPassword(email, password, function(e){
+      if(e){
+        that.displayError(e.reason);
+      } else {
+        FlowRouter.go('/dashboard');
+      }
+    });
+  },
   render (){
     return (
       <div>
@@ -6,7 +38,8 @@ Header = React.createClass({
           <i className="fa fa-facebook"></i>akebook
         </span>
         <div className="collapse navbar-collapse" id="navbar">
-          <form id="signin" className="navbar-form navbar-right" role="form">
+          <form onSubmit={this.handleSubmit} id="signin" role="form"
+            className="navbar-form navbar-right">
             <div className="input-group">
               <span className="input-group-addon">
                 <i className="fa fa-user"></i>
@@ -22,6 +55,8 @@ Header = React.createClass({
                 placeholder="Password" ref="password"/>
             </div>
             <button type="submit" className="btn btn-primary">Login</button>
+            <br/>
+            <span className={this.state.messageClass}>{this.state.message}</span>
           </form>
         </div>
       </div>
